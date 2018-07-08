0839ec7a871da7faaf6ad9c00a8d8363269dd0b1
okyk7 <okyk7@users.noreply.gitnub.com>
2018-07-08 16:36:41 +0900

    [update]docker-composeを最新に

--- docker-host/centos7/provision_root.sh
+++ docker-host/centos7/provision_root.sh
@@ -33,5 +33,5 @@ yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce
 yum -y install docker-ce
 systemctl start docker
 systemctl enable docker
-curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
